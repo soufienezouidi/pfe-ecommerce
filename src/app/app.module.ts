e67f5b5ee720ac587ed127819372632a345f2bc3
asilos <assil.jaber@esprit.tn>
2021-02-16 16:00:55 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule } from "@angular/material/toolbar";
+import {MatSidenavModule } from "@angular/material/sidenav";
+import {MatListModule } from "@angular/material/list";
+import {MatButtonModule } from "@angular/material/button";
+import {MatIconModule } from "@angular/material/icon";
+import { AppRoutingModule } from './app-routing';
+import { NavbarComponent } from "../app/utils/navbar/navbar.component";
+import { WavesModule, ButtonsModule, IconsModule } from 'angular-bootstrap-md'
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    WavesModule,
+     ButtonsModule, 
+     IconsModule, 
+    MatIconModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
